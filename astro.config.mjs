f807230198227a8db9df3f84a01da56fc3bfe0fc
hlope-dev <hlopetpr@gmail.com>
2026-01-09 16:09:16 -0500

    feat: Funcionalidad index y navegacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  prefetch: true,
+
+  integrations: [lottie()],
+});
